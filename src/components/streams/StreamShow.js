00650596ad604bddd6748292c7234f76034a6a96
roshita02 <roshitashakya07@gmail.com>
2019-12-12 22:11:16 +0545

    Show stream added

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -1,5 +1,25 @@
 import React from 'react';
-const StreamShow = () => {
-    return <div>Stream Show</div>
+import { connect } from 'react-redux';
+import { fetchStream } from '../../actions';
+
+class StreamShow extends React.Component {
+    componentDidMount(){
+        this.props.fetchStream(this.props.match.params.id);
+    }
+    render() {
+        if (!this.props.stream){
+            return <div>Loading...</div>
+        }
+        const { title, description } = this.props.stream;
+        return (
+            <div>
+                <h1>{title}</h1>
+                <h5>{description}</h5>
+            </div>
+        );
+    }
+}
+const mapStateToProps = (state, ownProps) => {
+    return { stream: state.streams[ownProps.match.params.id] };
 };
-export default StreamShow;
+export default connect(mapStateToProps, { fetchStream })(StreamShow);
